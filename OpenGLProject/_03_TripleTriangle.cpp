f8a50c4e14f0d494f8388c1ab6f0f9f4291bf44e
sonsungjong <tjdwhd70599@gmail.com>
2021-08-08 10:13:29 +0900

    Triple Triangle

--- OpenGLProject/_03_TripleTriangle.cpp
+++ OpenGLProject/_03_TripleTriangle.cpp
@@ -3,7 +3,7 @@
 #include <GLFW/glfw3.h>
 
 using namespace std;
-/*
+
 // Vertex Shader 소스코드
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
@@ -20,44 +20,59 @@ const char* fragmentShaderSource = "#version 330 core\n"
 "		FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
 "}\n\0";
 
-int main()
-{
+int main() {
 	// 초기화
 	glfwInit();
 
 	// 버전명시
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// CORE profile 명시
+	// Core profile 명시
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// OpenGL 전용 float
-	// shader란 OpenGL object.
-	// 삼각형 좌표
+	//GLfloat vertices[] =
+	//{
+	//	-0.5f, -0.5f*float(sqrt(3))/3, 0.0f,			// 하단 좌측
+	//	0.5f, -0.5f*float(sqrt(3))/3, 0.0f,				// 하단 우측
+	//	0.0f, 0.5f*float(sqrt(3))*2/3, 0.0f,			// 상단
+	//	-0.5f/2, 0.5f*float(sqrt(3))/6, 0.0f,			// 내부 좌측
+	//	0.5f/2, 0.5f*float(sqrt(3))/6, 0.0f,			// 내부 우측
+	//	0.0f, -0.5f*float(sqrt(3))/3, 0.0f,				// 내부 하단
+	//};
+
 	GLfloat vertices[] =
 	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,			// 하단 좌측
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,				// 하단 우측
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f			// 상단
+		-0.5f, -0.5f, 0.0f,			// 하단 좌측
+		0.5f, -0.5f, 0.0f,				// 하단 우측
+		0.0f, 0.5f, 0.0f,			// 상단
+		-0.25f, 0.0f, 0.0f,			// 내부 좌측
+		0.25f, 0.0f, 0.0f,			// 내부 우측
+		0.0f, -0.5f, 0.0f,				// 내부 하단
 	};
 
-	// GLFWwindow객체 생성
-	GLFWwindow* window = glfwCreateWindow(800, 800, "SungJong02", NULL, NULL);
-
-	// window 생성실패 체크
-	if (window == NULL)
+	// vertices를 가져다 쓸 좌표
+	GLuint indices[] =
 	{
+		0, 3, 5,			// 하단 좌측 삼각형 좌표 (vertices의 0행, 3,행 5행)
+		3, 2, 4,			// 상단 삼각형 (vertices의 3행, 2행, 4행)
+		5, 4, 1,			// 하단 우측 삼각형 (vertices의 5행, 4행, 1행)
+	};
+
+	// GLFWwindow 생성
+	GLFWwindow* window = glfwCreateWindow(800, 800, "SungJong03", NULL, NULL);
+
+	if (window == NULL) {		// window 생성실패 체크
 		cout << "GLFW window 생성 실패" << endl;
 		glfwTerminate();
 		return -1;
 	}
 
-	// 현재 context안에 window를 도입
+	// 현재 context 안에 window 객체 도입
 	glfwMakeContextCurrent(window);
 
 	// Load GLAD
 	gladLoadGL();
-	// Viewport 명시
+	// viewport 크기 지정
 	glViewport(0, 0, 800, 800);
 
 	// Vertex Shader Object와 그 레퍼런스를 생성한다
@@ -86,64 +101,62 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	// Vertex Array Object, Vertex Buffer Object 레퍼런스 컨테이너 생성
+	GLuint VAO, VBO, EBO;
 
-	// Vertex Array Object, Vertex Buffer Object
-	GLuint VAO, VBO;
-
-	// 각각 1씩 생성한다.
+	// VAO, VBO, EBO를 각 1개씩 생성
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
-	// 변수 VAO를 현재 Vertex Array Object로 만든다.
+	// 현재 Vertex Array Object를 변수 VAO로 만든다.
 	glBindVertexArray(VAO);
 
-	// 변수 VBO를 GL_ARRAY_BUFFER로 묶는다.
+	// Buffer 바인딩 후 vertices 데이터를 도입
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// VBO안에 vertices를 넣는다.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// OpenGL이 어떻게 VBO를 읽을지 정하기 위해 Vertex 속성을 구성한다
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
-	// OpenGL이 그것을 사용할 수 있도록 Vertex 속성을 허용한다.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	
+	// Buffer의 Vertex 속성을 지정
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// 잘못 수정하는 것을 방지하기 위해 VBO와 VAO를 0으로 묶는다.
+	// 잘못 수정하는 것을 방지하기 위해 0으로 묶음
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-
-	// 백그라운드 색상 명시
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	// back버퍼 제거와 새로운 색상 할당
-	glClear(GL_COLOR_BUFFER_BIT);
-	// back버퍼와 front버퍼 교체
-	glfwSwapBuffers(window);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	// Main loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// 백그라운드 색상
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		// back 버퍼 클린 후 새 색상 할당
 		glClear(GL_COLOR_BUFFER_BIT);
-		// Shader Program 사용
+		// 사용할 shader 명시
 		glUseProgram(shaderProgram);
-		// 사용할 VAO를 묶는다.
+		// VAO 바인딩
 		glBindVertexArray(VAO);
-		// GL_TRIANGLES로 삼각형을 그린다
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// GL_TRIANGLES를 사용해 삼각형 그리기
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		// back버퍼와 front버퍼 교체
 		glfwSwapBuffers(window);
-		// 모든 GLFW 이벤트를 처리하는 함수
+
+		// 모든 GLFW 이벤트 처리
 		glfwPollEvents();
 	}
 
-	// 모든 객체를 해제
+	// object 해제
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
-
-	// 프로그램 종료 전에 window창을 없앤다.
+	
+	// 프로그램 종료 전 window와 GLFW 해제
 	glfwDestroyWindow(window);
-	// 프로그램 종료 전에 GLFW를 제거한다.
 	glfwTerminate();
 
 	return 0;
 }
-*/
